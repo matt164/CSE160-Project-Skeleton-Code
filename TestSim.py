51f82d369760a0bc754c7f65741ca8e49faf814d
matt164 <53389439+matt164@users.noreply.github.com>
2021-09-20 20:09:59 -0700

    Update TestSim.py

--- TestSim.py
+++ TestSim.py
@@ -137,11 +137,16 @@ def main():
     s.bootAll();
     s.addChannel(s.COMMAND_CHANNEL);
     s.addChannel(s.GENERAL_CHANNEL);
+    s.addChannel(s.NEIGHBOR_CHANNEL);
+    s.addChannel(s.FLOODING_CHANNEL);
 
     s.runTime(20);
     s.ping(1, 2, "Hello, World");
     s.runTime(10);
     s.ping(1, 3, "Hi!");
+    s.runTime(20);
+    s.runTime(20);
+    s.neighborDMP(4);
     s.runTime(20);
 
 if __name__ == '__main__':
