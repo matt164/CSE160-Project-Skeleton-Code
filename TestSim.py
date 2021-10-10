9fa5a821304d16a42c85f325087ece2e64993fd9
matt164 <53389439+matt164@users.noreply.github.com>
2021-10-10 15:11:45 -0700

    Update TestSim.py

--- TestSim.py
+++ TestSim.py
@@ -136,7 +136,7 @@ def main():
     s.loadNoise("no_noise.txt");
     s.bootAll();
     #s.addChannel(s.COMMAND_CHANNEL);
-    s.addChannel(s.GENERAL_CHANNEL);
+    #s.addChannel(s.GENERAL_CHANNEL);
     #s.addChannel(s.NEIGHBOR_CHANNEL);
     #s.addChannel(s.FLOODING_CHANNEL);
 
